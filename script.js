8db510444c78e481e7c7e16980dd93148bae16db
Diaa-E <blarb21@yahoo.com>
2022-10-01 02:05:31 +0200

    Add book constructor and info function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function book(title, author, numberOfPages, isRead)
+{
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+};
+
+book.prototype.info = function()
+{
+    return `${title} by ${author}, ${numberOfPages} pages, ` +
+            `${isRead === true? "Already read" : "Not read Yet"}.`
+}

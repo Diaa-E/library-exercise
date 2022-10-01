fc562d67b548568c833d750569c77ce298568768
Diaa-E <blarb21@yahoo.com>
2022-10-01 19:25:06 +0200

    Add a function to toggle read status

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ function book(title, author, numberOfPages, isRead)
     this.isRead = isRead;
 };
 
+//return book's info
 book.prototype.info = function()
 {
-    return `${title} by ${author}, ${numberOfPages} pages, ` +
-            `${isRead === true? "Already read" : "Not read Yet"}.`
+    return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ` +
+            `${this.isRead === true? "Already read" : "Not read Yet"}.`
+}
+
+//read or unread a book
+book.prototype.toggleRead = function()
+{
+    this.isRead = !this.isRead;
 }
